e95aedb3b4ccda367971cd01a7fc2ab8ca2c06b6
aliismail527 <bcsf15m527@pucit.edu.pk>
2021-04-10 13:14:11 +0500

    First commit ..

---
+++ config/initializers/date_time_selector.rb
@@ -0,0 +1,31 @@
+module ActionView
+    module Helpers
+      class DateTimeSelector
+  
+        # Given an ordering of datetime components, create the selection HTML
+        # and join them with their appropriate separators.
+        def build_selects_from_types(order)
+          select = ""
+          order.reverse_each do |type|
+            separator = separator(type)
+            select.insert(0, separator.to_s + send("select_#{type}").to_s)
+          end
+          select.html_safe
+        end
+  
+        # Returns the separator for a given datetime component.
+        def separator(type)
+          return "" if @options[:use_hidden]
+          field_name = "#{@options[:prefix]}_#{@options[:field_name]}"
+          case type
+            when :year
+              "<label for='#{field_name}_1i'>Year</label>"
+            when :month
+              "<label for='#{field_name}_2i'>Month</label>"
+            when :day
+              "<label for='#{field_name}_3i'>Day</label>"
+          end
+        end
+      end
+    end
+  end
